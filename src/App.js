f1ff872f05c8497dfbadbab89d24de4245c09f2f
Josh <joshisadesigner@gmail.com>
2019-01-07 12:05:38 -0600

    Import map display component and add state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import locations from './data/locations.json';
+import MapDisplay from './components/MapDisplay';
 
 class App extends Component {
+    state = {
+        lat: 52.3693739,
+        lng: 4.8902479,
+        zoom: 14,
+        all: locations
+    };
+
     render() {
-        return <div className="App" />;
+        return (
+            <div className="App">
+                <div>
+                    <h1>Amsterdam Restaurants</h1>
+                </div>
+                <MapDisplay
+                    lat={this.state.lat}
+                    lng={this.state.lng}
+                    zoom={this.state.zoom}
+                    locations={this.state.all}
+                />
+            </div>
+        );
     }
 }
 
