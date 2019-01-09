9ce243903dffa306ca4895fdba1a5b91b8410a2b
Josh <joshisadesigner@gmail.com>
2019-01-09 17:48:03 -0600

    Add markers and infowindow function

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { InfoWindow, Marker, Map, GoogleApiWrapper } from 'google-maps-react';
 
 const MAP_KEY = 'AIzaSyAVSL9eG92K3W19jt0uIpoxW_lZGPdxfJs';
 
 class MapDisplay extends Component {
     state = {
-        map: null
+        map: null,
+        markers: [],
+        markerProps: [],
+        activeMarker: {},
+        activeMarkerProps: {},
+        showingInfoWindow: false
     };
 
     componentdidMount = () => {};
 
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            activeMarker: marker,
+            activeMarkerProps: props,
+            showingInfoWindow: true
+        });
+
     mapReady = (props, map) => {
         this.setState({ map });
+        this.updateMarkers(this.props.locations);
     };
+
+    updateMarkers = locations => {
+        let markerProps = [];
+        let markers = locations.map((location, index) => {
+            let mProps = {
+                key: index,
+                index,
+                name: location.name,
+                url: location.url,
+                location: location.location
+            };
+
+            markerProps.push(mProps);
+
+            return {
+                location: location.location
+            };
+        });
+
+        this.setState({ markers, markerProps });
+    };
+
+    createMarker = (lat, lng) => <Marker position={{ lat: lat, lng: lng }} />;
 
     render = () => {
         const style = {
@@ -25,6 +61,10 @@ class MapDisplay extends Component {
             lng: this.props.lng
         };
 
+        let amProps = this.state.activeMarkerProps;
+
+        console.log(amProps);
+
         return (
             <Map
                 role="application"
@@ -35,7 +75,26 @@ class MapDisplay extends Component {
                 style={style}
                 initialCenter={center}
                 onclick={this.closeInfoWindow}
-            />
+                mapTypeControl={false}
+            >
+                {this.state.markers.map((location, index) => (
+                    <Marker
+                        key={index}
+                        position={location.location}
+                        animation={this.props.google.maps.Animation.DROP}
+                        onClick={this.onMarkerClick}
+                        name={this.state.markerProps[index].name}
+                        url={this.state.markerProps[index].url}
+                    />
+                ))}
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                >
+                    <h4>{amProps.name}</h4>
+                    <p>{amProps.url}</p>
+                </InfoWindow>
+            </Map>
         );
     };
 }
