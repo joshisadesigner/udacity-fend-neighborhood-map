ebd6702da12956304aee91e8f9443ed5825ba4de
Josh <joshisadesigner@gmail.com>
2019-02-04 11:03:35 -0600

    Isolate infoWindow as component

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import Marker from './Marker';
-import { InfoWindow } from 'google-maps-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFoursquare } from '@fortawesome/free-brands-svg-icons';
+import MarkerInfo from './MarkerInfo';
 
 const MAP_KEY = 'AIzaSyAVSL9eG92K3W19jt0uIpoxW_lZGPdxfJs';
 
@@ -70,50 +68,11 @@ class MapDisplay extends Component {
                         animation={this.setAnimation(name)}
                     />
                 ))}
-
-                <InfoWindow
-                    marker={activeMarker}
+                <MarkerInfo
+                    activeMarker={activeMarker}
                     visible={showingInfoWindow}
                     onClose={closeInfoWindow}
-                >
-                    <div>
-                        {activeMarker && activeMarker.name ? (
-                            <h4>{activeMarker.name}</h4>
-                        ) : (
-                            ''
-                        )}
-                        {activeMarker && activeMarker.url ? (
-                            <p>
-                                <a
-                                    className="marker-info-url"
-                                    href={activeMarker.url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Restaurant website
-                                </a>
-                            </p>
-                        ) : (
-                            ''
-                        )}
-                        {activeMarker && activeMarker.images ? (
-                            <figure className="img-marker">
-                                <figcaption>Image from foursquare.</figcaption>
-                                <img
-                                    alt={activeMarker.name}
-                                    src={
-                                        activeMarker.images.items[0].prefix +
-                                        'cap100' +
-                                        activeMarker.images.items[0].suffix
-                                    }
-                                />
-                                <FontAwesomeIcon icon={faFoursquare} />
-                            </figure>
-                        ) : (
-                            ''
-                        )}
-                    </div>
-                </InfoWindow>
+                />
             </Map>
         );
     };
---
+++ src/components/MarkerInfo.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { InfoWindow } from 'google-maps-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFoursquare } from '@fortawesome/free-brands-svg-icons';
+
+class MarkerInfo extends Component {
+    render() {
+        const { activeMarker, visible, onClose, ...props } = this.props;
+
+        console.log('MarkerInfo.js:  ');
+        console.log(activeMarker);
+        console.log(visible);
+
+        return (
+            <InfoWindow
+                {...props}
+                marker={activeMarker}
+                visible={visible}
+                onClose={onClose}
+            >
+                <div>
+                    {activeMarker && activeMarker.name ? (
+                        <h4>{activeMarker.name}</h4>
+                    ) : (
+                        ''
+                    )}
+                    {activeMarker && activeMarker.url ? (
+                        <p>
+                            <a
+                                className="marker-info-url"
+                                href={activeMarker.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Restaurant website
+                            </a>
+                        </p>
+                    ) : (
+                        ''
+                    )}
+                    {activeMarker && activeMarker.images ? (
+                        <figure className="img-marker">
+                            <figcaption>Image from foursquare.</figcaption>
+                            <img
+                                alt={activeMarker.name}
+                                src={
+                                    activeMarker.images.items[0].prefix +
+                                    'cap100' +
+                                    activeMarker.images.items[0].suffix
+                                }
+                            />
+                            <FontAwesomeIcon icon={faFoursquare} />
+                        </figure>
+                    ) : (
+                        ''
+                    )}
+                </div>
+            </InfoWindow>
+        );
+    }
+}
+export default MarkerInfo;
